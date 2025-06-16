15ba4cc735ed02ec6e774b0c6107faadd53ef0e9
brunoMyguelDotCom <brunomyguelmarcall@gmail.com>
2025-06-16 16:11:13 -0300

    adicao 2º exercicio

---
+++ exercicio_02/src/Exercicio02.java
@@ -0,0 +1,18 @@
+/*
+Lista 03 - exercicio 02
+
+Enunciado:
+
+Em uma empresa, cada funcionário recebe mensalmente o salário bruto mais um prêmio de comissões. São 
+fornecidos os seguintes valores: salário bruto, prêmio, base do INSS, base do imposto de renda (IR) e suas 
+respectivas taxas. O demonstrativo de pagamento deve incluir o salário bruto, prêmio, rendimento, desconto 
+de IR e salário líquido.  
+O rendimento é a soma do salário bruto e do prêmio. O desconto do INSS aplica-se se o rendimento exceder a 
+base do INSS. O desconto do IR aplica-se se a diferença entre o rendimento e o desconto do INSS for maior que 
+a base do IR.
+*/
+public class Exercicio02 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
